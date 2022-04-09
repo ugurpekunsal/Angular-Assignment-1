485aa57704d28e5051a6bd4620b0e6b1c82ce2c5
Ugur Pekunsal <ugurpekunsal@gmail.com>
2022-04-09 12:58:53 -0400

    Created success alert component and added styling

--- src/app/warning-alert/warning-alert.component.ts
+++ src/app/warning-alert/warning-alert.component.ts
@@ -1,7 +1,14 @@
 import { Component } from "@angular/core";
 
 @Component({
-  selector: "app-warning-alert-component",
+  styles: [
+    `
+      h2 {
+        color: red;
+      }
+    `,
+  ],
+  selector: "app-warning-alert",
   templateUrl: "./warning-alert.component.html",
 })
 export class WarningAlertComponent {}
